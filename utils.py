fdb27fcc5617fdc7412e8413efbccab8d7faea16
acrucha <mcaa@cin.ufpe.br>
2022-09-14 18:20:21 -0300

    (chore): updating constants Co-authored-by: Tales Cunha <Miletinho@users.noreply.github.com>

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from asyncio import constants
 import numpy as np
 from typing import Final
 
@@ -8,13 +9,26 @@ _D:               Final[int]   = 30                          # Number of dimensi
 _START:           Final[int]   = -15                         # Start of X range
 _END:             Final[int]   = 15                          # End of X range
 _MP:              Final[float] = 0.005                       # Mutation Probability (GA)
-_MP_ES:           Final[float] = 0.5                         # Mutation Probability (ES)
+_MP_ES:           Final[float] = 0.005                       # Mutation Probability (ES)
 _CP:              Final[float] = 0.9                         # Crossover Probability
 _POP_SIZE:        Final[int]   = 2000                        # Population size
 _MAX_GENERATIONS: Final[int]   = 10000
 _EPSILON:         Final[float] = 1/5                         # Epsilon (update rule)
-_GLOBAL_THAO:     Final[float] = 1/np.sqrt(2*_D)            
-_LOCAL_THAO:      Final[float] = 1/np.sqrt(2*np.sqrt(_D))
+_GLOBAL_TAU:      Final[float] = 1/np.sqrt(2*_D)            
+_LOCAL_TAU:       Final[float] = 1/np.sqrt(2*np.sqrt(_D))
 _ROUND:           Final[int]   = 4
 _EXECUTIONS:      Final[int]   = 3
 _FITNESS:         Final[int]   = 0.001
+
+
+constants = {
+    "1": [_D, _START, _END, _FITNESS],
+    "2": [2, -100, 100, 0.0],
+    "3": [20, -5.12, 5.12, _FITNESS],
+}
+
+funcs = {
+    "1": "Ackley",
+    "2": "Schaffer",
+    "3": "Rastrigin"
+}
